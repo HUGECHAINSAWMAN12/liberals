44a34efd87a53fe751de908f639d083854a4d1fd
HUGECHAINSAWMAN12 <122472623+HUGECHAINSAWMAN12@users.noreply.github.com>
2023-01-11 09:49:50 -0700

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,6 @@
+name = input("Enter a name! ")
+verb = input("Enter a verb! ")
+adjective = input ("Enter an adjective! ")
+noun = input ("Enter a noun! ")
+print("There once was a "+noun+" called "+name+". This "+noun+" called "+name+" was a very "+adjective+" fellow! They lived a happy life as a mischevious little fellow!")
+print("One day, this little fella decided to" +verb+ "! After doing this, the little fella perished.")
